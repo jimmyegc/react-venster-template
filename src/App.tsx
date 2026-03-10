0ae966de4b4c216813fce69e22ed112eece14670
Jimmy García <jimmyegc@gmail.com>
2026-03-09 21:43:39 -0600

    03092026

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -17,12 +16,7 @@ function App() {
       </div>
       <h1 className="text-2xl font-bold mb-4">Welcome to Ollama v0</h1>
       <div className="card p-6 bg-green-100 rounded-xl">
-        <button
-          className="bg-orange-400 py-2 px-4 rounded-xl cursor-pointer hover:bg-orange-500"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          count is {count}
-        </button>
+        
       </div>
     </>
   );
